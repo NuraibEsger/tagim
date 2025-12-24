980a852dcdc4be65a0db97e4fd3d6f8c22dc9083
nuraibesger <nuraibesger@gmail.com>
2025-12-24 20:40:48 +0400

    feat: Implement Soft Delete using Global Query Filters and BaseEntity

--- Tagim.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Tagim.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -16,6 +16,21 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<BaseEntity>().HasQueryFilter(b => !b.IsDeleted);
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        ChangeTracker.DetectChanges();
+        foreach (var item in ChangeTracker
+                     .Entries<BaseEntity>()
+                     .Where(e => e.State == EntityState.Deleted))
+        {
+            item.State = EntityState.Modified;
+            item.Entity.IsDeleted = true;
+        }
+        
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
