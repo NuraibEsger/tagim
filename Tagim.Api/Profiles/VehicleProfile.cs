da98205df1d36f2957a35a40503c377ef665e8e1
nuraibesger <nuraibesger@gmail.com>
2026-02-06 14:09:38 +0400

    SocialProfile.cs and VehicleProfile.cs profiles created

---
+++ Tagim.Api/Profiles/VehicleProfile.cs
@@ -0,0 +1,24 @@
+using AutoMapper;
+using Tagim.Application.DTOs.Vehicle;
+using Tagim.Application.Features.Vehicles.Commands.CreateVehicle;
+using Tagim.Application.Features.Vehicles.Commands.UpdateVehicle;
+using Tagim.Domain.Common;
+
+namespace Tagim.Api.Profiles;
+
+public class VehicleProfile : Profile
+{
+    public VehicleProfile()
+    {
+        CreateMap<Vehicle, VehicleDto>()
+            .ForMember(dest => dest.SocialMedia, 
+                opt => opt.MapFrom(src => src.User.SocialMediaLinks!.Where(x => x.IsVisible)));
+        CreateMap<Vehicle, PublicVehicleDto>();
+        CreateMap<CreateVehicleCommand, Vehicle>()
+            .ForMember(v => v.LicensePlate,
+                opt => opt.MapFrom(v => v.LicensePlate.ToUpper()));
+        CreateMap<UpdateVehicleCommand, Vehicle>()
+            .ForMember(v => v.LicensePlate, 
+                opt => opt.MapFrom(v => v.LicensePlate.ToUpper()));
+    }
+}
