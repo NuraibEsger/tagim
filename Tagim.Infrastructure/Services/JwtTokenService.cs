370c324413f23b207162ec08de75a4758309e728
nuraibesger <nuraibesger@gmail.com>
2026-02-09 16:42:30 +0400

    user.FullName removed from ClaimTypes

--- Tagim.Infrastructure/Services/JwtTokenService.cs
+++ Tagim.Infrastructure/Services/JwtTokenService.cs
@@ -19,8 +19,7 @@ public class JwtTokenService(IConfiguration configuration) : ITokenService
         {
             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
             new Claim(ClaimTypes.Email, user.Email),
-            new Claim(ClaimTypes.Name, user.FullName),
-            new Claim(ClaimTypes.Role, user.Role.ToString())
+            new Claim(ClaimTypes.Role, user.Role.ToString()),
         };
 
         var tokenDescriptor = new SecurityTokenDescriptor
